d930c770ccf1a93e8859e4c2d821ce2da11e16b0
Limonchik-D <DavLimon4@yandex.com>
2025-03-09 09:28:43 +0200

    Add files via upload

---
+++ Task 2 Pr26/Point.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_Pr26
+{
+    class Point
+    {
+        private double x; 
+        private double y;
+        private static int count; //Подсчет количества точек
+
+
+        public Point()
+        {
+            x = 1.0;
+            y = 1.0;
+            count++;
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+            count++;
+        }
+        public static int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        static Point()
+        {
+            count = 0;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+        public double SetX(double x)
+        {
+            return this.x = x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public double SetY(double y)
+        {
+            return this.y = y;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Точки Х: {x}, У = {y}");
+        }
+    }
+}
---
+++ Task 2 Pr26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_Pr26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.Print();
+            Console.WriteLine();
+
+            Point point2 = new Point(4.6, 6.4);
+            point2.Print();
+            Console.WriteLine();
+
+            Point point3 = new Point(6.4, 9.4);
+            point3.Print();
+            Console.WriteLine();
+
+            Console.WriteLine($"Количество точек: {Point.Count}");
+            Console.ReadLine();
+        }
+    }
+}
