371bb6eaf6a8c1e66b190b341a754add5acbcb62
Limonchik David <Limonchik4da@outlook.com>
2025-03-09 09:26:36 +0200

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_26
+{
+    class Person
+    {
+        private int age; //Возраст человека
+        private static int retirementAge; //Пенсионный возраст
+        public static int RetirementAge //Проверка на ограничение возаста до 100 лет
+        {
+            get
+            {
+                return retirementAge;
+            }
+            set
+            {
+                if(value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Возраст должен быть в диапазоне от 1 до 100");
+                }   
+                retirementAge = value;
+            }
+        }
+
+        public Person()
+        {
+            age = 20;
+            RetirementAge = 60;
+
+        }
+        public Person(int age, int retirementAge)
+        {
+            this.age = age;
+            RetirementAge = retirementAge;
+        }
+        public string CheckRetirementStatus()
+        {
+            if (age >= retirementAge)
+            {
+                return "Человек уже на пенсии";
+            }
+            else
+            {
+                int result = retirementAge - age;
+                return $"До пенсии осталось: {result} лет" ;
+            }
+
+
+        }
+        public void Output()
+        {
+            Console.WriteLine( $"Возраст: {age}, Пенсионный возраст: {retirementAge}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Output();
+            Console.WriteLine(person.CheckRetirementStatus());
+            Console.WriteLine();
+
+            Person person1 = new Person(50, 65);
+            person1.Output();
+            Console.WriteLine(person1.CheckRetirementStatus());
+            Console.WriteLine();
+
+            Person person2 = new Person();
+            Person.RetirementAge = 70;
+            person2.Output();
+            Console.WriteLine(person2.CheckRetirementStatus());
+            
+            Console.ReadKey();
+
+        }
+    }
+}
